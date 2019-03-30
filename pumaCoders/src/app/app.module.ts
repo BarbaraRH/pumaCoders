1ee344cd83f2a14c9b9f722fc0b3514a3d5b4bcf
katherine <kretamales280@gmail.com>
2019-03-30 09:57:39 -0300

    Se agregaron componentes

--- pumaCoders/src/app/app.module.ts
+++ pumaCoders/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsComponent } from './components/us/us.component';
+import { VoluntaryComponent } from './components/voluntary/voluntary.component';
+import { RouterModule} from '@angular/router';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsComponent,
+    VoluntaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'home', component:HomeComponent},
+      {path:'us', component:UsComponent},
+      {path:'voluntary', component:VoluntaryComponent}
+    ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
